a31feb31d399c2f0de2880d8cd8ccfc798c33c2c
zhangliuxiao <2535737692@qq.com>
2024-06-15 18:19:08 +0800

    新增member会员模块

--- member/src/main/java/com/jiawa/train/member/config/TrainApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/TrainApplication.java
@@ -1,9 +1,11 @@
-package com.jiawa.train;
+package com.jiawa.train.member.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.jiawa")
 public class TrainApplication {
 
 	public static void main(String[] args) {
