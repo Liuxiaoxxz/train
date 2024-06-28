5b0e3a4650fd4c7ab7306b445b426d67a0ad7d0c
zhangliuxiao <2535737692@qq.com>
2024-06-28 17:22:46 +0800

    解决了axios的跨域问题,设置请求参数为json格式

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -1,15 +1,17 @@
 package com.jiawa.train.member.controller;
 
 import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.member.req.MemberLoginReq;
 import com.jiawa.train.member.req.MemberRegisterReq;
+import com.jiawa.train.member.req.MemberSendCodeReq;
+import com.jiawa.train.member.resp.MemberLoginResp;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/member")
 public class MemberController {
 	@Resource
 	private MemberService memberService;
@@ -24,8 +26,20 @@ public class MemberController {
 	}
 
 	@PostMapping("/register")
-	public CommonResp<Long> register(@Valid MemberRegisterReq req){
+	public CommonResp<Long> register(@Valid @RequestBody MemberRegisterReq req){
 		long register = memberService.register(req);
 		return new CommonResp<>(register);
+	}
+
+	@PostMapping("/send-code")
+	public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req){
+		memberService.sendCode(req);
+		return new CommonResp<>();
+	}
+
+	@PostMapping("/login")
+	public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req){
+		MemberLoginResp resp = memberService.login(req);
+		return new CommonResp<>(resp);
 	}
 }
--- web/src/main.js
+++ web/src/main.js
@@ -2,13 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import Antd, {notification} from 'ant-design-vue';
+
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
+import axios from 'axios';
 
-const app= createApp(App)
-app.use(Antd).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app');
+
+
 const icons = Icons;
-for(const i in icons){
-    app.component(i,icons[i]);
+for (const i in icons) {
+    app.component(i, icons[i]);
 }
+
+/**
+ * axios拦截器
+ */
+axios.defaults.baseURL = 'http://localhost:8000';
